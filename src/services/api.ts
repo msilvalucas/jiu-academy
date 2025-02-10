26a777546f3894a2b9399a4b620abaead4e6826b
Lucas Gomes <lucasgomess2109@gmail.com>
2025-02-10 20:49:11 -0300

    feat: change value API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Student } from '../types';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: API_URL,
